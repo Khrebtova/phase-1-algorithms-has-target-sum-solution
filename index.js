843e04b018e5ec71b6637e9515531177a1392c0f
Kseniia McFadden <mcfaddenkseniia@gmail.com>
2022-01-03 12:02:50 -0800

    commit tree 2ad1918b99ec51d6c95dbc77f9a9ac4d9c7b39a7

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
-function hasTargetSum(array, target) {
-  // Write your algorithm here
-}
+
+  function hasTargetSum(array, target) {
+    // create new object
+    const seenNumbers = {};
+    //itarate through an array
+    //O(n)
+    for (const number of array) {
+      // calcilate complement number O(n)
+      const complement = target - number;
+      // look for complement number in new object, if found return TRUE O(n)
+      if (seenNumbers[complement]) return true;
+      // if not fount add number to new object O(n)
+      seenNumbers[number] = true;
+    }
+    //for number matching complement number is not found return FALSE O(1)
+    return false;
+  }
 
 /* 
   Write the Big O time complexity of your function here
+  O(n)
 */
 
 /* 
-  Add your pseudocode here
+  create and object of seen/already checked elements
+  iterate through every element of and array,
+  then find a complement numbet for every element to meet the target 
+  check if new object has element what matches complement number, if not add element to new object., if yes, return TRUE.
+  after all array iteration if new object dont have elements matching taget input, returt FALSE
+
 */
 
 /*
